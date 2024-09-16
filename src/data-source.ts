513ae5aafd75e913c889d7285284049809a524ef
Saul Ramos <seramos@cleveritgroup.com>
2024-09-15 23:53:23 -0400

    optimize complex code

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,17 @@
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'user',
-  password: 'password',
-  database: 'handson_clean_code',
-  synchronize: true,
-  logging: false,
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'user',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'handson_clean_code',
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+  logging: process.env.DB_LOGGING === 'true',
   entities: ['src/entities/**/*.ts'],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],
